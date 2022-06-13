393b6ec7c7fe4aa2e875239c559b837deaedb6bf
sirsa4 <yaya.abbdi@gmail.com>
2022-06-13 18:03:59 +0200

    all methods basic setup to test on postman

--- controllers/taskControll.js
+++ controllers/taskControll.js
@@ -1,7 +1,30 @@
 //taskController.js
 
-const getTasks = (req,res)=>{
+//GET method controller for All tasks
+const getAllTasks = (req,res)=>{
     res.status(200).send(`<h1>Hello There! 😒</h1>`);
 }
 
-module.exports = {getTasks};
+//GET single
+const getTask = (req,res)=>{
+    res.status(200).json({id: req.params, complete: true});
+}
+
+//POST method controller
+const postTask = (req,res)=>{
+    const {name} = req.body;
+
+    res.status(200).send(`Welcome ${name} , you ae now posted on server 😊`);
+}
+
+//update/patch a single task
+const updateTask = (req,res)=>{
+    res.status(200).json({update: true, id: req.params});
+}
+
+//DELETE method controller
+const deleteTask = (req,res)=>{
+    res.status(200).json({delete: true, id: req.params});
+}
+
+module.exports = {getAllTasks, getTask, postTask, updateTask, deleteTask};
--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -4,8 +4,22 @@ const express = require("express");
 const taskRoute = express.Router();
 
 //import controller functions
-const {getTasks} = require("../controllers/taskControll");
+const {getAllTasks, getTask, postTask, updateTask, deleteTask} = require("../controllers/taskControll");
 
-taskRoute.get("/",getTasks);
+//GET route
+taskRoute.get("/",getAllTasks);
+
+//POST route
+taskRoute.post("/",postTask);
+
+//GET single task
+taskRoute.get("/:id", getTask);
+
+//PATCH route single task
+taskRoute.patch("/:id",updateTask);
+
+//DELETE route single task
+taskRoute.delete("/:id", deleteTask);
+
 
 module.exports = taskRoute;
